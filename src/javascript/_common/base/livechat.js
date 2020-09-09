8027a996a09204cbcd314df0fe992328eaba07c5
mustofa-binary <mustofa-binary@binary.com>
2020-09-09 12:31:58 +0800

    fix: change let to const to avoid re_used session variable

--- src/javascript/_common/base/livechat.js
+++ src/javascript/_common/base/livechat.js
@@ -4,6 +4,13 @@ const ClientBase = require('./client_base');
 const LiveChat = (() => {
     const init = () => {
         if (window.LiveChatWidget) {
+            // We have to clear the session first on livechat init to prevent
+            // getting data from previous session.
+            const blank_session_variables = { loginid: '', landing_company_shortcode: '', currency: '', residence: '' };
+            window.LiveChatWidget.call('set_customer_email', ' ');
+            window.LiveChatWidget.call('set_customer_name', ' ');
+            window.LiveChatWidget.call('set_session_variables', blank_session_variables);
+            
             BinarySocket.wait('get_settings').then((response) => {
                 const get_settings = response.get_settings || {};
                 const { first_name, last_name } = get_settings;
@@ -15,28 +22,26 @@ const LiveChat = (() => {
 
             window.LiveChatWidget.on('visibility_changed', ({ visibility }) => {
                 // only visible to CS
-                let session_variables = { loginid: '', landing_company_shortcode: '', currency: '', residence: '' };
-
                 if (visibility === 'maximized' && ClientBase.isLoggedIn()) {
                     const loginid = ClientBase.get('loginid');
                     const landing_company_shortcode = ClientBase.get('landing_company_shortcode');
                     const currency = ClientBase.get('currency');
                     const residence = ClientBase.get('residence');
 
-                    session_variables = {
+                    const current_session_variables = {
                         ...loginid && { loginid },
                         ...landing_company_shortcode && { landing_company_shortcode },
                         ...currency && { currency },
                         ...residence && { residence },
                     };
 
-                    window.LiveChatWidget.call('set_session_variables', session_variables);
+                    window.LiveChatWidget.call('set_session_variables', current_session_variables);
                 }
 
                 if (visibility === 'maximized' && !ClientBase.isLoggedIn()) {
                     window.LiveChatWidget.call('set_customer_email', ' ');
                     window.LiveChatWidget.call('set_customer_name', ' ');
-                    window.LiveChatWidget.call('set_session_variables', session_variables);
+                    window.LiveChatWidget.call('set_session_variables', blank_session_variables);
                 }
             });
         }
