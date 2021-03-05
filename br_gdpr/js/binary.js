0f31642dd8742ef541ffa6b473d9c8621d230a6d
mustofa-binary <mustofa@binary.com>
2021-03-05 14:33:02 +0800

    Deploy to gdpr

--- br_gdpr/js/binary.js
+++ br_gdpr/js/binary.js
@@ -2951,7 +2951,10 @@ var GTM = function () {
 
     var loadGTMElements = function loadGTMElements() {
         if (document.body) {
-            document.body.appendChild(createElement('noscript', { html: '\n                <iframe src="//www.googletagmanager.com/ns.html?id=GTM-MZWFF7" height="0" width="0" style={{display: \'none\', visibility: \'hidden\'}}></iframe>\n            ' }));
+            var noscript = createElement('noscript');
+            noscript.innerHTML = '<iframe src="//www.googletagmanager.com/ns.html?id=GTM-MZWFF7" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe>';
+
+            document.body.appendChild(noscript);
             document.body.appendChild(createElement('script', {
                 'data-cfasync': 'false',
                 html: '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="//www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-MZWFF7");'
